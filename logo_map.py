6d7f65b83cc9a1e523aa8eef55fdea8ec78342cb
daiagi <daiagi@gmail.com>
2023-05-24 21:52:51 +0700

    new svgs

---
+++ logo_map.py
@@ -0,0 +1,21 @@
+MintedOn = {
+    'koda': 'koda',
+    'eth': 'eth',
+    'btc': 'btc'
+}
+
+logo_map = {
+    MintedOn['koda']: {
+        'src': '../Koda._pink.svg',
+        'width': '224px'
+        },
+    MintedOn['eth']: {
+        'src': '../ethereum-eth.svg',
+        'width': '120px'
+    },
+    MintedOn['btc']: {
+        'src': '../bitcoin-btc-logo.svg',
+        'width': '120px'
+    }
+}
+    
